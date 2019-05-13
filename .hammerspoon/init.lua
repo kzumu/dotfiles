52514b35af863ab84d18549745114f8b1a9ed74e
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2019-05-13 12:25:45 +0900

    Binding left cmd, right

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -16,5 +16,17 @@ end
 hs.eventtap.new({25}, mousePress):start()
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.eventtap.keyStroke({"cmd"}, "[")
+  hs.alert.show("cmd alt ctrl")
 end)
+
+hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(e)
+  local flags = e:getFlags()
+  if flags.cmd and not (flags.alt or flags.shift or flags.ctrl or flags.fn) then
+      local keyCode = e:getKeyCode()
+      if keyCode == 0x37 then
+          hs.eventtap.event.newKeyEvent({'shift', 'ctrl'}, ';', true):post()
+      elseif keyCode == 0x36 then
+          hs.eventtap.event.newKeyEvent({'shift', 'ctrl'}, 'J', true):post()
+      end
+  end    
+end):start()
